476a0ecbb0eb11a148e53895e138264bf08ac2b0
Vikas Badoni <L049606@corpau.wbcau.westpac.com.au>
2016-11-18 08:42:16 +0530

    Second program

---
+++ HelloWorldTest.java
@@ -0,0 +1,6 @@
+public class HelloWorldTest {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("This is hello world test");
+   }
+}
